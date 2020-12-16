403baf3a24476514046a26d0dce838f8ea4fd06f
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-16 17:22:28 -0600

    added branding to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,26 @@
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
-// import Logo from '../img/ag-logo_clr.svg';
+import Logo from '../img/ag-logo_clr.svg';
 import './NavBar.scss'
 
 export const NavBar = () => {
     return (
         <Container>
-            <Navbar expand='lg' variant='light' bg='light'>
-                <Navbar.Brand href='#'>
-                    <h1>Aaron Gertler</h1>
+            <Navbar expand='lg' variant='light' bg='light' id='mainNav'>
+                <Navbar.Brand href='#' className='navBarLogo'>
+                    <img
+                        alt=""
+                        src={Logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+
+                    <h1 id='myName'>Aaron Gertler</h1>
                 </Navbar.Brand>
+
                 <Navbar.Toggle />
+
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
                         Signed in as: <a href="#login">Mark Otto</a>
