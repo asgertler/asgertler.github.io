813982e99c969ce4a02fdb0d46197a0a4791fc06
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-16 16:32:30 -0600

    implemented Bootstrap Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
-import Navbar from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
 import Logo from '../img/ag-logo_clr.svg';
 import './NavBar.scss'
 
 export const NavBar = () => {
     return (
-        <div className="appHeaderBranding">
-            <h1>Aaron Gertler</h1>
-            <h4>UI/UX Designer & Front-End Developer</h4>
-        </div>
+        <Navbar fixed='top' bg='dark' variant='dark' className="navBar">
+            <img src={Logo} alt='logo' className='navBarLogo' />
+
+            <div className="navBarName">
+                <h1>Aaron Gertler</h1>
+                <h4>UI/UX Designer & Front-End Developer</h4>
+            </div>
+        </Navbar>
     )
 }
