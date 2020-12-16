a2fff06e0c59be499d4f75f470738bae41764fb3
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-16 16:22:43 -0600

    moved nav elements to NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import Navbar from 'react-bootstrap'
-
 import Logo from '../img/ag-logo_clr.svg';
+import './NavBar.scss'
 
 export const NavBar = () => {
     return (
         <div className="appHeaderBranding">
             <h1>Aaron Gertler</h1>
+            <h4>UI/UX Designer & Front-End Developer</h4>
         </div>
     )
 }
