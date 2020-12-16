a820fd12e304d5697c6cbbddfe8c7794f211e1af
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-16 14:30:06 -0600

    initial setup complete

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
+    <div className="app">
+      <header className="appHeader">
+        <h1>AARON GERTLER</h1>
+        <h4>UI/UX Designer & Front-End Developer</h4>
       </header>
     </div>
   );
