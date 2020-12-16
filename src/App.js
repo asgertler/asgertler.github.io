d4795dc4199eaf2c2e354894e929fb15144b0d82
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-16 15:47:39 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
   return (
     <div className="app">
       <header className="appHeader">
-        <h1>AARON GERTLER</h1>
-        <h4>UI/UX Designer & Front-End Developer</h4>
+        <div className="appHeaderBranding">
+          <h1>AARON GERTLER</h1>
+          <h4>UI/UX Designer & Front-End Developer</h4>
+        </div>
       </header>
     </div>
   );
