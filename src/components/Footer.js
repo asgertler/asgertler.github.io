aba3db1c683cdbb4b5e7a5e7279bc30763300dac
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-17 12:21:10 -0600

    added Footer.js and included copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,26 +6,22 @@ import { FaGithub } from 'react-icons/fa'
 import { FaLinkedin } from 'react-icons/fa'
 import './Footer.scss'
 
+let today = new Date()
+let year = today.getFullYear()
+
 export const Footer = () => {
     return (
         <Navbar expand='lg' variant='light' bg='light' fixed='bottom' id='footer'>
             <Container>
-                <Navbar.Brand href='#' className='navBarLogo'>
-                    <h1 id='myName'>Aaron Gertler</h1>
-                </Navbar.Brand>
+                <Nav className='ml-auto mr-auto'>
+                    <Navbar.Text id='copyright'>
+                        Copyright © {year} Aaron Gertler. All Rights Reserved. |
+                    </Navbar.Text>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="#development">Development</Nav.Link>
-                        <Nav.Link href="#design">Design</Nav.Link>
-                        <div className='navBarSocial'>
-                            <Nav.Link href='https://github.com/asgertler' target='_blank'><FaGithub /></Nav.Link>
-                            <Nav.Link href='https://dribbble.com/aarongertler' target='_blank'><FaDribbble /></Nav.Link>
-                            <Nav.Link href='https://www.linkedin.com/in/asgertler/' target='_blank'><FaLinkedin /></Nav.Link>
-                        </div>
-                    </Nav>
-                </Navbar.Collapse>
+                    <Nav.Link href='https://github.com/asgertler' target='_blank'><FaGithub /></Nav.Link>
+                    <Nav.Link href='https://dribbble.com/aarongertler' target='_blank'><FaDribbble /></Nav.Link>
+                    <Nav.Link href='https://www.linkedin.com/in/asgertler/' target='_blank'><FaLinkedin /></Nav.Link>
+                </Nav>
             </Container>
         </Navbar>
     )
