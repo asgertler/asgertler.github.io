589bfaf7269a860b71d60b2bdd4c3b1e6b85714f
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-29 17:05:04 -0600

    adjusting global padding for grid

--- src/components/Design.js
+++ src/components/Design.js
@@ -17,53 +17,55 @@ import './Design.scss'
 
 export const Design = () => {
     return (
-        <Col className='designList' xs={12} md={6}>
-            <h4>UI/UX Skills</h4>
-            <hr />
+        <Col xs={12} md={6} className='skillsCol'>
+            <div className='designList'>
+                <h4>UI/UX Skills</h4>
+                <hr />
 
-            <Button size='sm'>
-                <SiAdobexd style={{ color: '#F75EEE', backgroundColor: '#450135' }} />&nbsp; Adobe XD
-            </Button>
+                <Button size='sm' variant='info'>
+                    <SiAdobexd style={{ color: '#F75EEE', backgroundColor: '#450135' }} />&nbsp; Adobe XD
+                </Button>
 
-            <Button size='sm'>
-                <FaPaintBrush style={{ color: 'mediumspringgreen' }} />&nbsp; Branding
-            </Button>
+                <Button size='sm' variant='info'>
+                    <FaPaintBrush style={{ color: 'mediumspringgreen' }} />&nbsp; Branding
+                </Button>
 
-            <Button size='sm'>
-                <FaPen style={{ color: 'gold' }} />&nbsp; Copywriting
-            </Button>
+                <Button size='sm' variant='info'>
+                    <FaPen style={{ color: 'gold' }} />&nbsp; Copywriting
+                </Button>
 
-            <Button size='sm'>
-                <SiFigma style={{ color: '#F24E1E' }} />&nbsp; Figma
-            </Button>
+                <Button size='sm' variant='info'>
+                    <SiFigma style={{ color: '#F24E1E' }} />&nbsp; Figma
+                </Button>
 
-            <Button size='sm'>
-                <SiAdobeillustrator style={{ color: '#F79500', backgroundColor: '#310000' }} />&nbsp; Illustrator
-            </Button>
+                <Button size='sm' variant='info'>
+                    <SiAdobeillustrator style={{ color: '#F79500', backgroundColor: '#310000' }} />&nbsp; Illustrator
+                </Button>
 
-            <Button size='sm'>
-                <SiAdobeindesign style={{ color: '#F73163', backgroundColor: '#47021E' }} />&nbsp; InDesign
-            </Button>
+                <Button size='sm' variant='info'>
+                    <SiAdobeindesign style={{ color: '#F73163', backgroundColor: '#47021E' }} />&nbsp; InDesign
+                </Button>
 
-            <Button size='sm'>
-                <FaSitemap style={{ color: 'cyan' }} />&nbsp; Journey Mapping
-            </Button>
+                <Button size='sm' variant='info'>
+                    <FaSitemap style={{ color: 'cyan' }} />&nbsp; Journey Mapping
+                </Button>
 
-            <Button size='sm'>
-                <SiAdobelightroomclassic style={{ color: '#2FA3F7', backgroundColor: '#001D34' }} />&nbsp; Lightroom
-            </Button>
+                <Button size='sm' variant='info'>
+                    <SiAdobelightroomclassic style={{ color: '#2FA3F7', backgroundColor: '#001D34' }} />&nbsp; Lightroom
+                </Button>
 
-            <Button size='sm'>
-                <SiAdobephotoshop style={{ color: '#2FA3F7', backgroundColor: '#001D34' }} />&nbsp; Photoshop
-            </Button>
+                <Button size='sm' variant='info'>
+                    <SiAdobephotoshop style={{ color: '#2FA3F7', backgroundColor: '#001D34' }} />&nbsp; Photoshop
+                </Button>
 
-            <Button size='sm'>
-                <FaWrench style={{ color: 'lightsteelblue' }} />&nbsp; Prototyping
-            </Button>
+                <Button size='sm' variant='info'>
+                    <FaWrench style={{ color: 'lightsteelblue' }} />&nbsp; Prototyping
+                </Button>
 
-            <Button size='sm'>
-                <FaUserAlt style={{ color: 'dodgerblue' }} />&nbsp; User Testing
-            </Button>
+                <Button size='sm' variant='info'>
+                    <FaUserAlt style={{ color: 'dodgerblue' }} />&nbsp; User Testing
+                </Button>
+            </div>
         </Col>
     )
 }
--- src/components/Tech.js
+++ src/components/Tech.js
@@ -20,65 +20,67 @@ import './Tech.scss'
 
 export const Tech = () => {
     return (
-        <Col className='techList' xs={12} md={6}>
-            <h4>Development Skills</h4>
-            <hr />
+        <Col xs={12} md={6} className='skillsCol'>
+            <div className='techList'>
+                <h4>Development Skills</h4>
+                <hr />
 
-            <Button size='sm'>
-                <SiBootstrap style={{ color: '#61dafb' }} />&nbsp; Bootstrap
+                <Button size='sm' variant='info'>
+                    <SiBootstrap style={{ color: '#61dafb' }} />&nbsp; Bootstrap
                 </Button>
 
-            <Button size='sm'>
-                <SiCss3 style={{ color: 'dodgerblue' }} />&nbsp; CSS3
+                <Button size='sm' variant='info'>
+                    <SiCss3 style={{ color: 'dodgerblue' }} />&nbsp; CSS3
                 </Button>
 
-            <Button size='sm'>
-                <SiFirebase style={{ color: '#FFCA28' }} />&nbsp; Firebase
+                <Button size='sm' variant='info'>
+                    <SiFirebase style={{ color: '#FFCA28' }} />&nbsp; Firebase
                 </Button>
 
-            <Button size='sm'>
-                <SiGithub style={{ color: '#fafafa' }} />&nbsp; GitHub
+                <Button size='sm' variant='info'>
+                    <SiGithub style={{ color: '#fafafa' }} />&nbsp; GitHub
                 </Button>
 
-            <Button size='sm'>
-                <SiHtml5 style={{ color: '#f06529' }} />&nbsp; HTML5
+                <Button size='sm' variant='info'>
+                    <SiHtml5 style={{ color: '#f06529' }} />&nbsp; HTML5
                 </Button>
 
-            <Button size='sm'>
-                <SiJavascript style={{ color: '#fcdc00' }} />&nbsp; Javascript
+                <Button size='sm' variant='info'>
+                    <SiJavascript style={{ color: '#fcdc00' }} />&nbsp; Javascript
                 </Button>
 
-            <Button size='sm'>
-                <SiLinuxfoundation style={{ color: '#0099cc' }} />&nbsp; Linux
+                <Button size='sm' variant='info'>
+                    <SiLinuxfoundation style={{ color: '#0099cc' }} />&nbsp; Linux
                 </Button>
 
-            <Button size='sm'>
-                <SiNodeDotJs style={{ color: '#83cd29' }} />&nbsp; Node.js
+                <Button size='sm' variant='info'>
+                    <SiNodeDotJs style={{ color: '#83cd29' }} />&nbsp; Node.js
                 </Button>
 
-            <Button size='sm'>
-                <SiReact style={{ color: '#61dafb' }} />&nbsp; React
+                <Button size='sm' variant='info'>
+                    <SiReact style={{ color: '#61dafb' }} />&nbsp; React
                 </Button>
 
-            <Button size='sm'>
-                <SiSass style={{ color: '#c69' }} />&nbsp; SASS
+                <Button size='sm' variant='info'>
+                    <SiSass style={{ color: '#c69' }} />&nbsp; SASS
                 </Button>
 
-            <Button size='sm'>
-                <SiSellfy style={{ color: '#00B5AD' }} />&nbsp; SemanticUI
+                <Button size='sm' variant='info'>
+                    <SiSellfy style={{ color: '#00B5AD' }} />&nbsp; SemanticUI
                 </Button>
 
-            <Button size='sm'>
-                <SiVisualstudiocode style={{ color: '#0078d7 ' }} />&nbsp; VSCode
+                <Button size='sm' variant='info'>
+                    <SiVisualstudiocode style={{ color: '#0078d7 ' }} />&nbsp; VSCode
                 </Button>
 
-            <Button size='sm'>
-                <SiWordpress style={{ color: '#00A0D2' }} />&nbsp; Wordpress
+                <Button size='sm' variant='info'>
+                    <SiWordpress style={{ color: '#00A0D2' }} />&nbsp; Wordpress
                 </Button>
 
-            <Button size='sm'>
-                <FaCheck style={{ color: '#08cf65' }} />&nbsp; Wrike
+                <Button size='sm' variant='info'>
+                    <FaCheck style={{ color: '#08cf65' }} />&nbsp; Wrike
                 </Button>
+            </div>
         </Col>
     )
 }
