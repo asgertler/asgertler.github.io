d38736e79e1a7e0cbf19bb425062adbdb9c86849
Aaron Gertler <aaron.gertler@gmail.com>
2021-01-12 15:51:12 -0600

    updated welcome card

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,11 +11,11 @@ export const Welcome = () => {
     return (
         <Container id='welcomeContainer'>
             <Row id='welcomeRow'>
-                <Col className='welcomeSides d-none d-md-block' sm={{ span: 3, order: 'last' }}>
+                <Col className='welcomeSides d-none d-xl-block' xl={{ span: 3 }}>
                     <img src={Avatar} alt='avatar' id='avatar' />
                 </Col>
 
-                <Col className="welcomeSides" sm={{ span: 9, order: 'first' }}>
+                <Col className="welcomeSides" xl={{ span: 9 }}>
                     <h2>
                         <span>Hi, I'm </span>
 
@@ -28,17 +28,10 @@ export const Welcome = () => {
                         />
                     </h2>
 
-                    <h4>I build the front-end with a focus on UI/UX.</h4>
+                    <h4>I use UI/UX to build the front-end.</h4>
 
                     <p className='d-none d-md-block'>
-                        Before beginning down the road to becoming a front-end developer,
-                        I spent many years as a multimedia designer working in a number of
-                        various industries. The creative field gave me many opportunities to
-                        collaborate and work in a team-based environment while solving all sorts
-                        of visual and interactive challenges. I have always had a strong interest
-                        in technology and user interaction, and development spoke to me as a career
-                        where I could combine my love of design with even more logical problem solving
-                        and practical solutions to provide high-quality experiences to users.
+
                     </p>
                 </Col>
             </Row>
