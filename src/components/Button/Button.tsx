5ade26bd5afe4400a67309ef0239e13872ec0379
Aaron Gertler <aaron.gertler@gmail.com>
2024-03-14 12:41:48 -0400

    feat(sb): begin creation of sb component library

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,36 +1,19 @@
 // import React from 'react';
-import './button.css';
+import { JSXElementConstructor, ReactElement } from 'react'
+import './button.sass'
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  primary?: boolean
+  backgroundColor?: string
+  icon?: ReactElement<SVGElement, string | JSXElementConstructor<unknown>>
+  label: string
+  onClick?: () => void
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
   primary = false,
-  size = 'medium',
   backgroundColor,
+  icon,
   label,
   ...props
 }: ButtonProps) => {
@@ -38,11 +21,12 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
+      { icon ? icon : ''}
       {label}
     </button>
-  );
-};
+  )
+}
