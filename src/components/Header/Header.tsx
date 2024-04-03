8c3958f884a2df1c2eb25d4c693e73708baf9939
Aaron Gertler <aaron.gertler@gmail.com>
2024-04-03 18:41:08 -0500

    chore(db): organized imgs into more folders

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { NamePlate } from "../NamePlate/NamePlate"
 import { NavBar } from "../NavBar/NavBar"
-import Logo from '../../imgs/ag-logo_grn-blu.png'
+import Logo from '../../imgs/logos/ag-logo_grn-blu.png'
 import './header.sass'
 
 export const Header = () => {
