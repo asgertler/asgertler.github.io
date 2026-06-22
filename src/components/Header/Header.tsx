fc09fcdb670450aa1a92cd3e69178c30729ac861
Aaron Gertler <aaron.gertler@pm.me>
2026-06-22 18:37:38 -0500

    feat: updated logo treatments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import { NamePlate } from "../NamePlate/NamePlate"
 import { NavBar } from "../NavBar/NavBar"
-import Logo from '../../imgs/logos/ag_red.svg'
+import Logo from '../../imgs/logos/ag-logo_wordmark.svg'
 import './header.sass'
 
 export const Header = () => {
     return (
         <header>
-            <NamePlate logo={Logo} name='Design Engineer' />
+            <NamePlate logo={Logo} name='Aaron Gertler' />
             <NavBar />
         </header>
     )
